1936cfbe6c1790ae045b6ca226e994f8e81c3928
bekiassefa <118681825+bekiassefa@users.noreply.github.com>
2023-07-23 11:57:49 -0700

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,33 +1,27 @@
 #!/usr/bin/python3
 """
-Script that starts a Flask web application
+flask model
 """
-
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
-
 app = Flask(__name__)
-app.strict_slashes = False
 
 
 @app.teardown_appcontext
-def close_db(self):
+def teardown_data(self):
     """
-    Remove current SQLAlchemy Session
+        refrech data
     """
     storage.close()
 
 
-@app.route('/states_list')
-def list_of_states():
-    """
-    Displays all states present in DBStorage
-    """
-    return render_template('7-states_list.html',
-                           state=storage.all(State).values())
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ return all states in the db  """
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host='0.0.0.0', port=5000)
