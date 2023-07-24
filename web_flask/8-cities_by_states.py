182001e3fbf25ba85c47c580c15d5154f0e57099
bekiassefa <118681825+bekiassefa@users.noreply.github.com>
2023-07-24 08:41:10 -0700

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
+"""Start web application with two routings
 """
-flask model
-"""
-from flask import Flask, render_template
+
 from models import storage
 from models.state import State
+from flask import Flask, render_template
 app = Flask(__name__)
-storage.all()
+
+
+@app.route('/cities_by_states')
+def states_list():
+    """Render template with states
+    """
+    path = '8-cities_by_states.html'
+    states = storage.all(State)
+
+    # sort State object alphabetically by name
+    # sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template(path, states=states)
 
 
 @app.teardown_appcontext
-def teardown_data(self):
-    """
-        refrech data
+def app_teardown(arg=None):
+    """Clean-up session
     """
     storage.close()
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """ return all citie in the db  """
-    states = storage.all(State)
-
-    return render_template('8-cities_by_states.html', states=states)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
