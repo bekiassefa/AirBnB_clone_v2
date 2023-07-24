fc282dec1d6189f326209d80e90cbee68e8020c0
bekiassefa <118681825+bekiassefa@users.noreply.github.com>
2023-07-24 08:18:29 -0700

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
-"""
-Starts a Flask web application.
-"""
+"""Importing Flask to run the web app"""
 from flask import Flask, render_template
-from models import storage, State
+from models import storage
+from models.state import State
+
 
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """Displays a HTML page with a list of all State objects from the DBStorage."""
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_states)
+@app.route("/states_list", strict_slashes=False)
+def display_states():
+    """Render state_list html page to display States created"""
+    states = storage.all()
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown_session(exception):
-    """Removes the current SQLAlchemy Session after each request."""
+def teardown(self):
+    """Method to remove current SQLAlchemy Session"""
     storage.close()
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
-
